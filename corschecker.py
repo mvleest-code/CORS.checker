2be93ca916fb47a055c51c22efa29647841a22b6
Mark van Leest <53072185+mvleest-code@users.noreply.github.com>
2023-11-21 12:17:15 +0100

    Updated media endpoints by default
    Updated media endpoints by default

--- corschecker.py
+++ corschecker.py
@@ -7,54 +7,91 @@ app = Flask(__name__)
 @app.route('/check_cors', methods=['POST'])
 def check_cors():
     data = request.get_json()
-    url = data['url']
     domains = data['domain'].split(',')
 
-    parsed_url = urlparse(url)
-    mediaBaseUrl = parsed_url.netloc
+    urls = [
+        'https://media.c000.eagleeyenetworks.com',
+        'https://media.c001.eagleeyenetworks.com',
+        'https://media.c003.eagleeyenetworks.com',
+        'https://media.c004.eagleeyenetworks.com',
+        'https://media.c005.eagleeyenetworks.com',
+        'https://media.c006.eagleeyenetworks.com',
+        'https://media.c007.eagleeyenetworks.com',
+        'https://media.c012.eagleeyenetworks.com',
+        'https://media.c013.eagleeyenetworks.com',
+        'https://media.c014.eagleeyenetworks.com',
+        'https://media.c015.eagleeyenetworks.com',
+        'https://media.c016.eagleeyenetworks.com',
+        'https://media.c017.eagleeyenetworks.com',
+        'https://media.c018.eagleeyenetworks.com',
+        'https://media.c019.eagleeyenetworks.com',
+        'https://media.c020.eagleeyenetworks.com',
+        'https://media.c021.eagleeyenetworks.com',
+        'https://media.c022.eagleeyenetworks.com',
+        'https://media.c023.eagleeyenetworks.com',
+        'https://media.c024.eagleeyenetworks.com',
+        'https://media.c025.eagleeyenetworks.com',
+        'https://media.c026.eagleeyenetworks.com',
+        'https://media.c027.eagleeyenetworks.com'
+    ]
 
-    results = {}
+    all_results = []
 
-    for domain in domains:
-        domain = domain.strip()  # Remove leading/trailing whitespace
+    for url in urls:
+        parsed_url = urlparse(url)
+        mediaBaseUrl = url
 
-        headers = {
-            'authority': mediaBaseUrl,
-            'accept': '/',
-            'accept-language': 'en',
-            'access-control-request-headers': 'authorization',
-            'access-control-request-method': 'GET',
-            'origin': domain,
-            'referer': domain,
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-site',
-        }
+        domain_results = {}
 
-        try:
-            response = requests.options(url, headers=headers)
-        except requests.exceptions.RequestException as e:
-            results[domain] = {'error': str(e)}
-            continue
+        for domain in domains:
+            domain = domain.strip()
+            domain = domain.rstrip('/')
 
-        if ('access-control-allow-origin' in response.headers and 
-            response.headers['access-control-allow-origin'] == domain and 
-            'access-control-allow-credentials' in response.headers and 
-            response.headers['access-control-allow-credentials'] == 'true'):
-            results[domain] = {'result': 'CORS WHITELIST CHECK SUCCESS'}
-        else:
-            results[domain] = {'result': 'CORS WHITELIST CHECK FAILED'}
+            if not domain.startswith('http://') and not domain.startswith('https://'):
+                domain = 'https://' + domain
 
-    return jsonify(results)
+            headers = {
+                'authority': parsed_url.netloc,
+                'accept': '/',
+                'accept-language': 'en',
+                'access-control-request-headers': 'authorization',
+                'access-control-request-method': 'GET',
+                'origin': domain,
+                'referer': domain,
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'same-site',
+            }
+
+            try:
+                response = requests.options(url, headers=headers)
+                print(f"Check for {domain} on {url}: {response.status_code}")
+            except requests.exceptions.RequestException as e:
+                print(f"Error for {domain} on {url}: {e}")
+                domain_results[domain] = {'error': str(e)}
+                continue
+
+            if ('access-control-allow-origin' in response.headers and
+                    response.headers['access-control-allow-origin'] == domain and
+                    'access-control-allow-credentials' in response.headers and
+                    response.headers['access-control-allow-credentials'] == 'true'):
+                domain_results[domain] = {'result': 'CORS WHITELIST CHECK SUCCESS'}
+            else:
+                domain_results[domain] = {'result': 'CORS WHITELIST CHECK FAILED'}
+
+        all_results.append({url: domain_results})
+
+    return jsonify(all_results)
 
 @app.route('/')
 def home():
     return render_template_string('''
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>CORS whitelist checker</title>
-          <style>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CORS whitelist checker</title>
+    <link rel="icon" href="https://www.m-cloud.nl/favicon.ico">
+    <style>
         body {
             font-family: Roboto, sans-serif;
             margin: 0;
@@ -62,15 +99,15 @@ def home():
             display: flex;
             justify-content: center;
             align-items: center;
-            height: 100vh;
+            min-height: 100vh;
             background-color: #F7F8FC;
         }
         .container {
             text-align: center;
-            padding: 20px;
             background-color: #fff;
             box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
             width: 450px;
+            min-height: 215px;
         }
         input[type="text"] {
             margin: 10px 0;
@@ -85,49 +122,102 @@ def home():
             border: none;
             cursor: pointer;
         }
-        button:hover {
+        button:disabled {
+            background-color: #ccc;
+            cursor: not-allowed;
+        }
+        button:hover:enabled {
             background-color: #0082C3;
             color: #E3E9EF;
         }
+        #results-container {
+            margin-left: 10px;
+            margin-top: 10px;
+            margin-bottom: 10px;
+            width: 800px;
+            min-height: 215px;
+            max-height: 800px;
+            overflow-y: scroll;
+            background-color: #fff;
+            box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
+            position: relative;
+        }
+        .result {
+            margin-bottom: 1px;
+        }
     </style>
-        <script>
-            function checkCORS() {
-                const url = document.getElementById('url').value;
-                const domain = document.getElementById('domain').value;
+<script>
+async function checkCORS() {
+    const domain = document.getElementById('domain').value;
+    let resultsContainer = document.getElementById('results-container');
+    let checkCORSButton = document.getElementById('check-cors-button');
 
-                fetch('/check_cors', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({url: url, domain: domain}),
-                })
-                .then(response => response.json())
-                .then(data => {
-                    let message = '';
-                    for (const [domain, result] of Object.entries(data)) {
-                        message += domain + ': ' + (result.error || result.result) + '\\n';
-                    }
-                    alert(message);
-                })
-                .catch(error => {
-                    alert('Error: ' + error.message);
-                });
+    checkCORSButton.disabled = true;
+
+    try {
+        const response = await fetch('/check_cors', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ domain: domain }),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const responseBody = await response.text();
+
+        const data = JSON.parse(responseBody);
+
+        resultsContainer.innerHTML = '';
+
+        for (const resultItem of data) {
+            const url = Object.keys(resultItem)[0];
+            const domains = resultItem[url];
+
+            let labelDiv = document.createElement('div');
+            labelDiv.textContent = `Results for ${url}:`;
+            labelDiv.style.fontWeight = 'bold';
+            resultsContainer.appendChild(labelDiv);
+
+            for (const [domain, result] of Object.entries(domains)) {
+                let resultDiv = document.createElement('div');
+                resultDiv.className = 'result';
+
+                const resultText = result.error
+                    ? `Error: ${result.error}`
+                    : `Result: ${result.result.trim()}`;
+
+                resultDiv.textContent = `${domain}: ${resultText}`;
+                resultDiv.style.color = result.result && result.result.trim() === 'CORS WHITELIST CHECK SUCCESS' ? 'green' : 'red';
+                resultsContainer.appendChild(resultDiv);
             }
-        </script>
-    </head>
-    <body>
-        <div class="container">
-        <h1>CORS Checker v1.2</h1>
-        <label for="url">Enter MEDIA URL:</label><br>
-        <input type="text" id="url" value="https://media.c000.eagleeyenetworks.com"><br>
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    } finally {
+        checkCORSButton.disabled = false;
+    }
+}
+</script>
+</head>
+<body>
+    <div class="container">
+        <h1>CORS Checker v1.4</h1>
         <label for="domain">Enter CORS Domain(s):</label><br>
         <input type="text" id="domain" placeholder="Enter domains separated by commas"><br>
-        <button onclick="checkCORS()">Check CORS</button>
+        <button id="check-cors-button" onclick="checkCORS()">Check CORS</button>
+    </div><br>
+    
+    <div id="results-container">
+        <label for="results-container">Press "Check CORS" to show the results here.</label>
     </div>
-    </body>
-    </html>
+</body>
+</html>
+
     ''')
 
 if __name__ == '__main__':
- app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
